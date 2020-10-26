3e0a35dcb260a667c17775b28bfbf0e6afa36f64
Kemal Yenilmez <kemalyen@gmail.com>
2020-10-26 20:22:46 +0000

    Tempate model update

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -35,6 +35,11 @@ class Template extends Model
     public function getPlaceholdersListAttribute()
     {
         $return = null;
+        
+        if (!is_array($this->placeholders)){
+            return null;
+        }
+
         foreach($this->placeholders as $placeholder){
             $return .= $placeholder . ', ';
         }
